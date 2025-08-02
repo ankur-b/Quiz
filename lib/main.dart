de728be2f2c3514c0ff551e7c1ce73ab811f72ea
ankur-b <ankurbarve29@gmail.com>
2025-08-02 23:02:23 +0530

    start screen completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,5 +2,23 @@ import 'package:quiz_app/start_screen.dart';
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(const MaterialApp(home: Scaffold(body: StartScreen())));
+  runApp(
+    MaterialApp(
+      home: Scaffold(
+        body: Container(
+          decoration: const BoxDecoration(
+            gradient: LinearGradient(
+              colors: [
+                Color.fromARGB(255, 3, 169, 240),
+                Color.fromARGB(255, 2, 35, 60),
+              ],
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight
+            ),
+          ),
+          child: const StartScreen(),
+        ),
+      ),
+    ),
+  );
 }
